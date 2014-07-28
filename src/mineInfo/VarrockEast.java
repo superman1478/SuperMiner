2974dea817e6fb5452e9c6d4cf69c74394410b74
superman1478 <superman1478@gmail.com>
2014-07-28 01:29:02 -0500

    Added more tiles to VarrockEast.

--- src/mineInfo/VarrockEast.java
+++ src/mineInfo/VarrockEast.java
@@ -23,11 +23,12 @@ import tasks.WalkToMine;
 public class VarrockEast extends AreaInfo {
 	
 	private final Tile[] tilesToBank = new Tile[] {new Tile(3285, 3362, 0), new Tile(3287, 3372, 0),
-			new Tile(3291, 3382, 0), new Tile(3290, 3392, 0), new Tile(3290, 3402, 0),
-			new Tile(3290, 3405, 0), new Tile(3287, 3415, 0), new Tile(3285, 3425, 0),
-			new Tile(3275, 3427, 0), new Tile(3265, 3428, 0), new Tile(3219, 3444, 0),
+			new Tile(3293, 3378, 0), new Tile(3292, 3383, 0), new Tile(3292, 3387, 0),
+			new Tile(3290, 3392, 0), new Tile(3290, 3399, 0), new Tile(3290, 3405, 0),
+			new Tile(3287, 3415, 0), new Tile(3285, 3425, 0), new Tile(3275, 3427, 0),
+			new Tile(3269, 3428, 0), new Tile(3265, 3428, 0), new Tile(3219, 3444, 0),
 			new Tile(3259, 3428, 0), new Tile(3254, 3420, 0)};
-	
+
 	private final Filter<GameObject> rockFilter = new Filter<GameObject>() {
 		@Override
 		public boolean accept(GameObject o) {
@@ -41,7 +42,7 @@ public class VarrockEast extends AreaInfo {
 
 	@Override
 	public void addTasks(ArrayList<Task> tasks) {
-		
+
 		if (script().pickupOreOnGround()) {
 			tasks.add(new Pickup(ctx));
 		}
